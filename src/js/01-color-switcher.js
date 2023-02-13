3ae5be1555887b534dc624527bba963364b214a1
buencholo <david_arq.violin@yahoo.com>
2023-02-13 13:14:15 -0500

    task_one_done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+const bodyColor = document.querySelector("body")
 
+let intervalTimeRandom;
+
+console.log(btnStart, btnStop);
+
+btnStart.addEventListener("click", startRandomColor);
+
+function startRandomColor() {
+
+    intervalTimeRandom = setInterval(() => {
+
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        }
+        
+        bodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+        
+    }, 1000)}
+
+btnStop.addEventListener(`click`, stopRandomColor)
+
+function stopRandomColor() {
+clearInterval(intervalTimeRandom);
+};
